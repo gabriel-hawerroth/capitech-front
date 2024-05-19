e120ac3cc9b35f21306384759770a6b6e2534cdf
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-05-19 00:12:21 -0300

    feat: initial project configurations

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  mode: 'production',
+  production: true,
+  baseApiUrl:
+    'https://cloud-leaf.senior.com.br/t/senior.com.br/bridge/1.0/rest/my_domain/my_service/',
+};
