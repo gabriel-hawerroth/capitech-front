1d739046c029e1ea612e133b124c9f1be97d1986
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-03 13:51:49 -0300

    ci: added updateProd script

--- updateProd.sh
+++ updateProd.sh
@@ -1,9 +1,9 @@
 npm run build
 
-ssh root@147.79.81.13 "pm2 delete ssr.capitech"
+ssh root@15.229.18.114 "pm2 delete ssr.capitech"
 
-ssh root@147.79.81.13 "rm -rf /var/www/html/capitech/*"
+ssh root@15.229.18.114 "rm -rf /var/www/html/capitech/*"
 
-scp -r dist/capitech-front/* root@147.79.81.13:/var/www/html/capitech/
+scp -r dist/capitech-front/* root@15.229.18.114:/var/www/html/capitech/
 
-ssh root@147.79.81.13 "/root/start_capitech.sh"
+ssh root@15.229.18.114 "/root/start_capitech.sh"
