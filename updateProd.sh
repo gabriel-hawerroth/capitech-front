e56b7dbcd7084ef2e02aa7427fbbbe1b7857db29
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-22 13:27:38 -0300

    chore: use ssh alis

--- updateProd.sh
+++ updateProd.sh
@@ -1,9 +1,9 @@
 npm run build
 
-ssh ubuntu@168.138.150.229 "pm2 delete ssr.capitech"
+ssh ubuntu@hawetec "pm2 delete ssr.capitech"
 
-ssh ubuntu@168.138.150.229 "rm -rf /home/ubuntu/prd_projects/front/capitech/*"
+ssh ubuntu@hawetec "rm -rf /home/ubuntu/prd_projects/front/capitech/*"
 
-scp -r dist/capitech-front/* ubuntu@168.138.150.229:/home/ubuntu/prd_projects/front/capitech/
+scp -r dist/capitech-front/* ubuntu@hawetec:/home/ubuntu/prd_projects/front/capitech/
 
-ssh ubuntu@168.138.150.229 "/home/ubuntu/start_scripts/start_capitech.sh"
+ssh ubuntu@hawetec "/home/ubuntu/start_scripts/start_capitech.sh"
