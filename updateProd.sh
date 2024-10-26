3448b3fcaeffdd17c02815ffa5717de5be070ef0
Gabriel Hawerroth <gabrielhawerroth04@gmail.com>
2024-10-26 15:31:43 -0300

    ci: remove updateProd script

--- updateProd.sh
+++
@@ -1,9 +0,0 @@
-npm run build
-
-ssh ubuntu@hawetec "pm2 delete ssr.capitech"
-
-ssh ubuntu@hawetec "rm -rf /home/ubuntu/prd_projects/front/capitech/*"
-
-scp -r dist/capitech-front/* ubuntu@hawetec:/home/ubuntu/prd_projects/front/capitech/
-
-ssh ubuntu@hawetec "/home/ubuntu/start_scripts/start_capitech.sh"
